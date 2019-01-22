435414dfd0f73f46de16542e5e672d094d8b13c8
Rudolph Barbu <Rudolph_Barbu@outlook.com>
2019-01-22 13:04:36 +0200

    Added controllers classes

---
+++ Server/src/main/java/com/inther/controller/MessageController.java
@@ -0,0 +1,9 @@
+package com.inther.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController
+{
+
+}
--- Server/src/main/java/com/inther/domain/User.java
+++ Server/src/main/java/com/inther/domain/User.java
@@ -1,9 +1,17 @@
 package com.inther.domain;
 
-import javax.persistence.Entity;
 
-//Entity
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
 public class User
 {
+    @Id
+    @GeneratedValue(generator = "userIdGenerator")
+    @SequenceGenerator(name = "userIdGenerator", sequenceName = "question_sequence")
+    @Column(name = "USER_ID")
+    private int userId;
+
 
 }
