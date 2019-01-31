c435089ddb5c096f2ef2403354cafc9a53395fe7
Stanislav <st4s1k24@gmail.com>
2019-01-31 15:30:57 +0200

    merge

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -6,11 +6,8 @@ import { EditPresentationComponent } from './edit-presentation/edit-presentation
 import { HomeComponent } from './home/home.component';
 import { PresentationPageComponent } from './presentation-page/presentation-page.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-<<<<<<< HEAD
 import { PresentationDetailResolverService } from './services/presentation-detail-resolver.service';
-=======
 import { AuthGuard } from './_guards/auth.guard';
->>>>>>> fbb950323db589193751efe91c73a8d663224a56
 
 
 
@@ -18,7 +15,6 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
-<<<<<<< HEAD
   { path: 'new-presentation', component: EditPresentationComponent },
   {
     path: 'edit-presentation/:id',
@@ -34,11 +30,9 @@ const routes: Routes = [
       presentation: PresentationDetailResolverService
     }
   },
-=======
   { path: 'new-presentation', component: EditPresentationComponent, canActivate: [AuthGuard] },
   { path: 'edit-presentation/:id', component: EditPresentationComponent },
   { path: 'presentation-page/:id', component: PresentationPageComponent },
->>>>>>> fbb950323db589193751efe91c73a8d663224a56
   { path: 'user-profile/:id', component: UserProfileComponent }
 ];
 
--- Client/src/app/services/authentication.service.ts
+++ Client/src/app/services/authentication.service.ts
@@ -1,96 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment.prod';
-<<<<<<< HEAD
 import { UserDTO } from '../dto/user.dto';
-=======
-import { Observable } from 'rxjs';
-import { UserDTO } from '../dto/user.dto';
-import { GlobalServUserService } from '../global-serv-user.service';
->>>>>>> fbb950323db589193751efe91c73a8d663224a56
 
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    // 'Content-Type': 'multipart/form-data',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    // 'Access-Control-Allow-Credentials': 'true'
-  })
-};
 const API_URL = environment.apiUrl;
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthenticationService {
-  constructor(private http: HttpClient, private globalSrv: GlobalServUserService) { }
-  public authSuccess = false;
-  userLocal: UserDTO;
-  // RealBack
+  constructor(private http: HttpClient) { }
 
-  // login(email: string, password: string) {
+  login(email: string, password: string) {
+    //   return this.http.post<any>(`${API_URL}/users/authenticate`, { email, password })
 
-  //   const fd = new FormData();
+    const fd = new FormData();
 
-  //   fd.append('email', email);
-  //   fd.append('password', password);
+    fd.append('email', email);
+    fd.append('password', password);
 
-  //   // working part
-  //   // return this.http.post<any>(`${API_URL}/authentication`, JSON.stringify({ email: email, password: password }), httpOptions)
-  //   // return this.http.post<any>(`${API_URL}/authentication`, fd, httpOptions)
-  //   return this.http.post<any>(`${API_URL}/authentication`, fd)
-
-<<<<<<< HEAD
-    return this.http.post<any>(`${API_URL}/authenticate`, {email: email, password: password}, {
+    return this.http.post<any>(`${API_URL}/userLogin`, {email: email, password: password}, {
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+        'Access-Control-Allow-Headers': '*',
       }
     })
-=======
-  //     .pipe(map(user => {
-  //       // login successful if there's a user in the response
-  //       if (user) {
-  //         // store user details and basic auth credentials in local storage
-  //         // to keep user logged in between page refreshes
-  //         user.authdata = window.btoa(email + ':' + password);
-  //         localStorage.setItem('currentUser', JSON.stringify(user));
-  //         alert('Success!');
-  //         alert(user);
-  //         this.userLocal = JSON.parse(localStorage.getItem('currentUser'));
-  //         this.globalSrv.setNavEmail(this.userLocal.email);
-  //         this.globalSrv.setNavUserId(this.userLocal.id);
-  //         localStorage.setItem('auth', JSON.stringify(true));
-  //         const authe = JSON.parse(localStorage.getItem('auth'));
-  //         this.globalSrv.setNavAuthenticated(authe);
-  //       }
-  //       // alert()
-  //       return user;
-  //     }));
-  // }
-
-  // Fake backend
-  login(email: string, password: string) {
-    return this.http.post<any>(`${API_URL}/users/authenticate`, { email, password })
->>>>>>> fbb950323db589193751efe91c73a8d663224a56
       .pipe(map(user => {
         // login successful if there's a user in the response
         if (user) {
           // store user details and basic auth credentials in local storage
           // to keep user logged in between page refreshes
-          user.authdata = window.btoa(email + ':' + password);
-          localStorage.setItem('currentUser', JSON.stringify(user));
+          // user.authdata = window.btoa(email + ':' + password);
           alert('Success!');
-          this.userLocal = JSON.parse(localStorage.getItem('currentUser'));
-          this.globalSrv.setNavEmail(this.userLocal.email);
-          this.globalSrv.setNavUserId(this.userLocal.id);
-          localStorage.setItem('auth', JSON.stringify(true));
-          const authe = JSON.parse(localStorage.getItem('auth'));
-          this.globalSrv.setNavAuthenticated(authe);
+          localStorage.setItem('currentUser', JSON.stringify(user));
         }
 
         return user;
@@ -100,8 +43,5 @@ export class AuthenticationService {
   logout() {
     // remove user from local storage to log user out
     localStorage.removeItem('currentUser');
-    // localStorage.removeItem('auth');
-    localStorage.setItem('auth', JSON.stringify(false));
-    this.globalSrv.setNavAuthenticated(false);
   }
 }
