8eb9b53a309e5f9fde6a3a0f297ce5797eecff5d
Rudolph Barbu <rudolph_barbu@outlook.com>
2019-02-14 11:35:23 +0200

    Email system inegrated

--- Server/src/main/java/com/inther/services/entity/PresentationService.java
+++ Server/src/main/java/com/inther/services/entity/PresentationService.java
@@ -1,11 +1,14 @@
 package com.inther.services.entity;
 
+import com.inther.entities.Participant;
 import com.inther.entities.Presentation;
 import com.inther.entities.User;
 import com.inther.repositories.PresentationRepository;
 import com.inther.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,19 +22,39 @@ public class PresentationService
     private final PresentationRepository presentationRepository;
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final MailSender mailSender;
 
     @Autowired
     public PresentationService(PresentationRepository presentationRepository,
                                UserRepository userRepository,
-                               ModelMapper modelMapper)
+                               ModelMapper modelMapper, MailSender mailSender)
     {
         this.presentationRepository = presentationRepository;
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.mailSender = mailSender;
+    }
+
+    private void sendNotificationMessages(Presentation presentation, String subject, String text)
+    {
+        for (Participant participant : presentation.getParticipants())
+        {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setTo(participant.getEmail());
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(text);
+            mailSender.send(simpleMailMessage);
+        }
     }
 
     public Presentation newPresentation(Presentation presentation)
     {
+        sendNotificationMessages(presentation, "You has been invited on presentation",
+                "Presentation name: " + presentation.getTitle()
+                        + "n/Presentation description: " + presentation.getDescription()
+                        + "n/n/Presentation start time: " + presentation.getStartTime()
+                        + "n/n/Presentation end time: " + presentation.getStartTime()
+                        + "n/n/Presentation place: " + presentation.getPlace());
         return presentationRepository.save(presentation);
     }
 
@@ -67,6 +90,9 @@ public class PresentationService
 
     public Optional<Boolean> deletePresentation(UUID id)
     {
+        Optional<Presentation> optionalPresentation = presentationRepository.findPresentationById(id);
+        optionalPresentation.ifPresent(presentation -> sendNotificationMessages(presentation, "Presentation was canceled",
+                "Presentation '" + presentation.getTitle() + "' was canceled"));
         return presentationRepository.findPresentationById(id)
                 .map(p -> {
                     presentationRepository.deletePresentationById(id);
